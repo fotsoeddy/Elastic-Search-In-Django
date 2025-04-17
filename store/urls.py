81793538eee389f7e784950cc157d4d85076d711
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-04-17 12:44:32 +0100

    Refactor:Make in such a way that the page should not refresh please

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'store'
+
 urlpatterns = [
-    path('', views.index, name='product_search'),
+    path('', views.index, name='index'),
+    path('search', views.SearchJsonView.as_view(), name='search_json'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,49 +1,42 @@
+from django.views import View
 from django.shortcuts import render
+from django.http import JsonResponse
 from elasticsearch_dsl import Search
 from elasticsearch_dsl.query import MultiMatch
 from .documents import BrandDocument, SupplierDocument, ProductDocument, CategoryDocument
 
 def index(request):
-    q = request.GET.get("q")
     context = {}
-    
-    if q:
-        # Define the multi-match query
-        query = MultiMatch(
-            query=q,
-            fields=['name', 'email', 'phone', 'description'],
-            fuzziness='AUTO'
-        )
-        
-        # Create a multi-index search
-        search = Search(index=['products', 'suppliers', 'brands', 'categories']).query(query)
-        
-        # Execute the search
-        response = search.execute()
-        
-        # Organize results by document type
-        products = []
-        suppliers = []
-        brands = []
-        categories = []
-        
-        for hit in response:
-            if hit.meta.index == 'products':
-                products.append(hit)
-            elif hit.meta.index == 'suppliers':
-                suppliers.append(hit)
-            elif hit.meta.index == 'brands':
-                brands.append(hit)
-            elif hit.meta.index == 'categories':
-                categories.append(hit)
-        
-        # Add results to context
-        context.update({
-            'products': products,
-            'suppliers': suppliers,
-            'brands': brands,
-            'categories': categories,
-            'query': q
-        })
-    
     return render(request, 'index.html', context)
+
+class SearchJsonView(View):
+    def get(self, request):
+        q = request.GET.get("q", "")
+        results = {"products": [], "suppliers": [], "brands": [], "categories": [], "query": q}
+        
+        if q:
+            query = MultiMatch(
+                query=q,
+                fields=['name', 'email', 'phone', 'description'],
+                fuzziness='AUTO'
+            )
+            search = Search(index=['products', 'suppliers', 'brands', 'categories']).query(query)
+            response = search.execute()
+            
+            for hit in response:
+                item = {
+                    "name": getattr(hit, "name", ""),
+                    "description": getattr(hit, "description", ""),
+                    "email": getattr(hit, "email", ""),
+                    "phone": getattr(hit, "phone", ""),
+                }
+                if hit.meta.index == 'products':
+                    results["products"].append(item)
+                elif hit.meta.index == 'suppliers':
+                    results["suppliers"].append(item)
+                elif hit.meta.index == 'brands':
+                    results["brands"].append(item)
+                elif hit.meta.index == 'categories':
+                    results["categories"].append(item)
+        
+        return JsonResponse(results)
